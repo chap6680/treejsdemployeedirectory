459878767ae7e93b8b9950ee7aa0b4e386eef8c2
david chapman <chap6680@gmail.com>
2018-06-09 16:01:30 -0600

    working - playing with modal fades

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 let getA = [];
 modalString = '';
+modalString2 = '';
+getArrayLocation = 0;
+
 
 function getN() {
 	fetch('https://randomuser.me/api/?results=12')
@@ -32,7 +35,15 @@ function calcme(num) {
 	return num;
 };
 
-function createItemDiv(element, classN) {
+function createDiv(element, classN, tempno) {
+	tempID = 'item-' + tempno;
+	let tempx = document.createElement(element); // Create the type of element you pass in the parameters
+	tempx.id = tempID;
+	tempx.className = classN;
+	return tempx;
+}
+
+function createItemDiv(element, classN, tempno) {
 	let tempx = document.createElement(element); // Create the type of element you pass in the parameters
 	tempx.className = classN;
 	return tempx;
@@ -59,10 +70,13 @@ console.log('getA:  ', getA);
 
 function rungetA() {
 	console.log('start rungetA');
+	let tempno = 0;
 	getA.map(function (author) {
-		let creatediv = createItemDiv('div', 'item'),
+
+		console.log('count: ',tempno);
+		let creatediv = createDiv('div', 'item', tempno),
 			img = createNode('img'),
-			createdivitem = createItemDiv('div', 'itemtext'),
+			createdivitem = createItemDiv('div', 'itemtext',),
 			spanName = createNode('p');
 		spanEmail = createNode('p');
 		spanState = createNode('p');
@@ -79,6 +93,7 @@ function rungetA() {
 
 		append(creatediv, createdivitem);
 		append(group, creatediv);
+		tempno += 1;
 	});
 }
 
@@ -113,15 +128,38 @@ console.log(tempX); */
 
 function modaldata() { 
 	modalString = '<div><img src="images/emppic1.png">	<h1>Modal Example</h1>	</div><div>	<p>david.chapman@asdf.com</p>	<p>Denver</p>	<hr>	<p>314-313-3283</p>	<p>7741 Cornell Ave, St Louis MO 63105</p>	<p>Birthday: 10/22/1971</p>	</div>'
-}
+};
+
+function modalFromArray(getArrayLocation) {
+	modalString = `<div>
+	<img src=${getA[getArrayLocation].picture.large}>
+	<h1>${getA[getArrayLocation].name.first} ${getA[getArrayLocation].name.last}</h1>
+</div><div>	<p>${getA[getArrayLocation].email}</p>	<p>${getA[getArrayLocation].location.city}</p>	<hr>	<p>${getA[getArrayLocation].phone}</p>	<p>${getA[getArrayLocation].location.street} ${getA[getArrayLocation].location.city}, ${getA[getArrayLocation].location.state} ${getA[getArrayLocation].location.postcode}</p>	<p>Birthday: ${getA[getArrayLocation].dob}</p>	</div>`
+};
+
+modalString2 = '<div><img src="images/emppic1.png">	<h1>Modal Example</h1>	</div><div>	<p>dc@asdf.com</p>	<p>Denver</p>	<hr>	<p>999-999-2332</p>	<p>7741 Cornell Ave, Denver CO 99999</p>	<p>Birthday: 10/22/2000</p>	</div>'
 
 var modal = document.querySelector("#modal");
 var modalOverlay = document.querySelector("#modal-overlay");
-var closeButton = document.querySelector("#close-button");
+var closeButton = document.getElementById("close-button");
+var forwardButton = document.getElementById("forward-button");
+var backButton = document.getElementById("back-button");
 var openButton = document.querySelector("#open-button");
 var openButtonArray = document.querySelector("#open-button-array");
-var modalDataElement = document.getElementById('modal-data');
+let modalDataInfo = document.getElementById('modal-data');
+var updateDataButton = document.querySelector('#data-button');
+var gridSelector = document.getElementById("container");
 
+
+forwardButton.addEventListener("click", function () {
+	getArrayLocation += 1;
+	advanceModal();
+});
+
+backButton.addEventListener("click", function () {
+	getArrayLocation -= 1;
+	advanceModal();
+});
 
 closeButton.addEventListener("click", function () {
 	modal.classList.toggle("closed");
@@ -129,16 +167,49 @@ closeButton.addEventListener("click", function () {
 });
 
 openButton.addEventListener("click", function () {
+	modalFromArray(getArrayLocation);
+	modalDataInfo.innerHTML=modalString;
 	modal.classList.toggle("closed");
 	modalOverlay.classList.toggle("closed");
 });
 
-modalDataElement.addEventListener("click", function () {
-modalDataElement.innerHTML=modalString;
-});
+function openModal() { 
+	modalFromArray(getArrayLocation);
+	modalDataInfo.innerHTML=modalString;
+	modal.classList.toggle("closed");
+	modalOverlay.classList.toggle("closed");
+}
+function advanceModal() { 
+/* 	modalOverlay.classList.toggle("fade");
+ */	modal.classList.toggle("fade");
+	modalFromArray(getArrayLocation);
+	
+	setTimeout(function(){
+		modalDataInfo.innerHTML = modalString;
+	}, 500);
+        setTimeout(function(){
+/* 			modalOverlay.classList.toggle("fade");
+ */			modal.classList.toggle("fade");
+		
+        }, 1000);
+};
 
-openButtonArray.addEventListener("click", function () {
+
+/* openButtonArray.addEventListener("click", function () {
 	alert(getA[3].dob);
+}) */;
+gridSelector.addEventListener("click", function (e) {
+//	alert('clicked');
+/* 	let clickedItem = e.target;
+	console.log('clicked item: ', clickedItem);
+ */
+	getArrayLocation = Number(e.target.id.split("-").pop());
+	openModal();
+	/* console.log('clicked item id: ', e.target.id, " net to: ", getArrayLocation);
+	console.log(getA[getArrayLocation]) */;
+
+
+	//	alert("Hello " + clickedItem);
 });
 
 
